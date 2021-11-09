5d1f24dc220232f29a2372219914c62eb0267b64
selfire1 <try2banonym@gmail.com>
2021-11-09 10:39:41 +0100

    refactor: ♻️ Rename plugin class

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface SlashPlugin {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: SlashPlugin = {
 	mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: SlashPlugin;
 
 	async onload() {
 		await this.loadSettings();
